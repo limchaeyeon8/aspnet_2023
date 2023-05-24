22cc1077ecea3140fa82adee3d78a6f89bd23e24
limchaeyeon8 <2000goal2001@gmail.com>
2023-05-24 11:19:52 +0900

    .

---
+++ d6/Day06_Web/aspnet02_boardapp/Models/RegisterModel.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace aspnet02_boardapp.Models
+{
+    public class RegisterModel
+    {
+        // 회원가입할 때 데이터 받는 부분
+
+        [Required]
+        [EmailAddress]
+        [DisplayName("---Email Address---")]
+        public string Email { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        [DisplayName("---Password---")]
+        public string Password { get; set; }
+        [DataType(DataType.Password)]
+        [DisplayName("---Password Check---")]
+        [Compare("Password", ErrorMessage = "***패스워드가 일치하지 않습니다! 다시 입력하세요***")]
+        public string ConfirmPassword { get; set; }
+    }
+}
